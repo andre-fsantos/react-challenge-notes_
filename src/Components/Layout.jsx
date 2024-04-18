938b9522b24cc675ad973c871fcb5779b84b869b
André Santos <afds.santoss@gmail.com>
2024-04-18 10:56:37 -0300

    Remove fragment e altera o path do svg btnClose

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -3,26 +3,24 @@ import './Layout.css';
 
 const Layout = () => {
     return (
-        <>
-            <main>
-                <aside>
-                    <div className="box-input-title">
-                        <input type="text" placeholder="Title" className="inputTitle"/>
-                    </div>
-                    <div className="box-textarea">
-                        <textarea placeholder="Content"></textarea>
-                    </div>
-                    <div>
-                        <button>Add Note</button>
-                    </div>
-                </aside>
-                <div className="notes">
-                    <Note />
-                    <Note />
-                    <Note />
+        <main>
+            <aside>
+                <div className="box-input-title">
+                    <input type="text" placeholder="Title" className="inputTitle" />
                 </div>
-            </main>
-        </>
+                <div className="box-textarea">
+                    <textarea placeholder="Content"></textarea>
+                </div>
+                <div>
+                    <button>Add Note</button>
+                </div>
+            </aside>
+            <div className="notes">
+                <Note />
+                <Note />
+                <Note />
+            </div>
+        </main>
     )
 }
 
--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -1,19 +1,16 @@
 import './Note.css';
-import btnClose from './../../public/btnClose.svg';
 
 const Note = () => {
     return (
-        <>
-            <div className="note">
-                <div className='box-btn-close'>
-                    <img src={btnClose}/>
-                </div>
-                <h2>Note 1</h2>
-                <div className='box-content'>
-                    <p>This is the content for note 1</p>
-                </div>
+        <div className="note">
+            <div className='box-btn-close'>
+                <img src='/btnClose.svg' />
             </div>
-        </>
+            <h2>Note 1</h2>
+            <div className='box-content'>
+                <p>This is the content for note 1</p>
+            </div>
+        </div>
     )
 }
 
