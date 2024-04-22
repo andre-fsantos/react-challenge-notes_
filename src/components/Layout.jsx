70bcdd0b1008b623603fac9b3ab1685808fa6181
André Santos <afds.santos@hotmail.com>
2024-04-22 00:46:12 -0300

    Adiciona nota

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,25 @@
 import { Note } from "./Note"
-import './Layout.css';
 import { getNotes } from "../infrastructure/getNotes";
+import { saveNote } from "../infrastructure/saveNote";
+import { Toast } from "./Toast";
 import { useEffect, useState } from "react";
+import './Layout.css';
 
 const Layout = () => {
     const [notes, setNotes] = useState([]);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [dataToast, setDataToast] = useState({});
+    const [isToastVisible, setIsToastVisible] = useState(false);
+
 
     const fecthGetNotes = async () => {
         try {
-            const data = await getNotes();
-            setNotes(data);
+            const notes = await getNotes();
+            const orderedNotes = [...notes].reverse();
+            setNotes(orderedNotes);
         } catch (error) {
-            console.log('erro');
+            throw new Error(error);
         }
     }
 
@@ -19,17 +27,48 @@ const Layout = () => {
         fecthGetNotes();
     }, []);
 
+
+    const showToast = dataToast => {
+        const TOAST_DURATION = 3000;
+        setDataToast(dataToast);
+        setIsToastVisible(true);
+
+        setTimeout(() => {
+            setIsToastVisible(false);
+        }, TOAST_DURATION);
+    }
+
+
+    const addNote = async () => {
+        try {
+            const response = await saveNote({ title, description });
+            if(response.id !== undefined) showToast({ message: 'Nota adicionada!', bgColor: '#a7f5a7' });
+            
+            setTitle('');
+            setDescription('');
+            fecthGetNotes();
+        } catch(error) {
+            throw new Error(error);
+        }
+    }
+
     return (
         <main>
+            {
+                isToastVisible &&
+                <Toast
+                    dataToast={dataToast}
+                />
+            }
             <aside>
                 <div className="box-input-title">
-                    <input type="text" placeholder="Title" className="inputTitle" />
+                    <input type="text" placeholder="Title" className="inputTitle" value={title} onChange={e => setTitle(e.target.value)}/>
                 </div>
                 <div className="box-textarea">
-                    <textarea placeholder="Content"></textarea>
+                    <textarea placeholder="Content" value={description} onChange={e => setDescription(e.target.value)}></textarea>
                 </div>
                 <div>
-                    <button>Add Note</button>
+                    <button onClick={addNote}>Add Note</button>
                 </div>
             </aside>
             <div className="notes">
